8965bee56e0a8b994d71bb4a0bc479bfd674b038
Camilo318 <camiloa0318@gmail.com>
2021-05-23 14:37:36 -0500

    Remove mikasa asset. Update postcss and package-lock version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import mikasa from './assets/mikasa.png'
 import styles from './style.css'
 const App = ({ title }) => (
   <div>
     <h2 className={styles.title}>{title}</h2>
-    <img src={mikasa} alt='Mikasa sprite' />
   </div>
 )
 
