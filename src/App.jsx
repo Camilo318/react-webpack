cfebf800f95071ba0894afb6d22691e40133b555
Camilo318 <camiloa0318@gmail.com>
2021-05-23 20:06:31 -0500

    Disable some ESLint rules. Add env config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './style.css'
 
+/* eslint-disable react/prop-types */
+
 const App = ({ title }) => (
   <div>
     <h2 className={styles.title}>{title}</h2>
